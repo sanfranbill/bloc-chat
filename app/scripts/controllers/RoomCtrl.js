812cd4df3cc61eb06b96e96fb40888ee5c47f991
sanfranbill <laptopbatteryusa@gmail.com>
2017-10-26 04:32:27 -0700

    finished checkpoint 4

--- app/scripts/controllers/RoomCtrl.js
+++ app/scripts/controllers/RoomCtrl.js
@@ -1,9 +1,15 @@
 (function() {
-  function RoomCtrl(Room) {
-      this.all = Room.all;
-  }
+    function RoomCtrl(Room) {
+        this.all = Room.all;
+        /** Establish the selected room with the following function. */
+        this.setRoom = function(clickedRoom) {
+            this.selectedRoom = clickedRoom;
+            this.messages = Message.getByRoomId(this.selectedRoom.$id);
+            console.log(this.messages);
+        };     
+    }
 
-  angular
-    .module('blocChat')
-    .controller('RoomCtrl', ["Room", RoomCtrl]);
+    angular
+        .module('blocChat')
+        .controller('RoomCtrl', ["Room", RoomCtrl]);
 })();
---
+++ app/scripts/services/message.js
@@ -0,0 +1,21 @@
+(function() {
+    function Message($firebaseArray) {
+        var Message = {};
+        var ref = firebase.database().ref().child("messages");
+        var messages = $firebaseArray(ref);
+        
+        /** This func takes the selected room and shows all its messages. "orderByChild" method reveals the messages logged in the firebase database of the selected room. Its chained together with the chile() method that is here represented by the 'ref' variable. */
+        Message.getByRoomId = function(roomId) {
+            // filter messages by their room ID
+            var messages =$firebaseArray(ref.orderByChild('roomId')) .equalTo(roomId));
+            console.log(roomId);
+            return messages;
+        }
+        
+        return Message;
+    };
+
+    angular
+        .module('blocChat')
+        .factory('Message', ['$firebaseArray', Message]);
+})();
